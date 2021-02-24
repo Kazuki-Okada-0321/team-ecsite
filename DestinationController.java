20b200e645a3ac0f3b559e873708947c28237909
Kazuki-Okada-0321 <nissyokada23@gmail.com>
2021-02-24 18:49:25 +0900

    チーム開発で作成した宛先情報登録機能

---
+++ DestinationController.java
@@ -0,0 +1,75 @@
+package jp.co.internous.crocus.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import jp.co.internous.crocus.model.domain.MstDestination;
+import jp.co.internous.crocus.model.mapper.MstDestinationMapper;
+import jp.co.internous.crocus.model.mapper.MstUserMapper;
+import jp.co.internous.crocus.model.session.LoginSession;
+import jp.co.internous.crocus.model.domain.MstUser;
+import jp.co.internous.crocus.model.form.DestinationForm;
+
+@Controller
+@RequestMapping("/crocus/destination")
+public class DestinationController {
+	
+	@Autowired
+	private MstUserMapper userMapper;
+
+	@Autowired
+	private LoginSession loginSession;
+
+	@Autowired
+	 MstDestinationMapper destinationMapper;
+	
+	private Gson gson = new Gson();
+	
+	@RequestMapping("/")
+	public String index(Model m) {
+		
+		MstUser user = userMapper.findByUserNameAndPassword(loginSession.getUserName(),loginSession.getPassword());
+		
+		m.addAttribute("user", user);
+		// page_header.htmlでsessionの変数を表示させているため、loginSessionも画面に送る。
+		m.addAttribute("loginSession",loginSession);
+		return "destination";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/delete")
+	@ResponseBody
+	public boolean delete(@RequestBody String destinationId) {
+		
+		Map<String, String> map = gson.fromJson(destinationId, Map.class);
+		String id = map.get("destinationId");
+
+		int result = destinationMapper.logicalDeleteById(Integer.parseInt(id));
+
+		return result > 0;
+	}
+	
+	@RequestMapping("/register")
+	@ResponseBody
+	public String register(@RequestBody DestinationForm f) {
+		
+		MstDestination destination = new MstDestination(f);
+		int userId = loginSession.getUserId();
+		destination.setUserId(userId);
+		int count = destinationMapper.insert(destination);
+		
+		Integer id = 0;
+		if (count > 0) {
+			id = destination.getId();
+		}
+		return id.toString();
+	}
+}
